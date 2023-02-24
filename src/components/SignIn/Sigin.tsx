358bed9a1fec36fac03e191bfd6b0ec2029fa350
harry-sketch <harshbisht90@gmail.com>
2023-02-24 13:24:38 +0530

    add: signin ui

---
+++ src/components/SignIn/Sigin.tsx
@@ -0,0 +1,16 @@
+const SignIn = () => {
+  
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <button
+        type="button"
+        className="rounded-xl border border-slate-400 p-2.5 text-2xl font-bold text-slate-50"
+      >
+        SigIn Using Gitub
+      </button>
+    </div>
+  );
+};
+
+export default SignIn;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#246b4d",
+        bg: "#1c1c1c",
+      },
+    },
   },
   plugins: [],
 };
